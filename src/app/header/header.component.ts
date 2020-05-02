1dd61ef2090f7e71c45ff9b63b04ae387369fa1c
Ward Muylaert <ward.muylaert@gmail.com>
2020-05-02 22:05:51 +0200

    Clean up carousel code
    - Remove duplication, use *ngFor instead
    - Make the image size names consistent enough and ensure those images
      exist
    - Beautify HTML
    - Beautify CSS
    - Add some comments to the different sections
    - Remove inschrijving link from hamburger menu too, until it works

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,19 +5,27 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  
 })
 
 export class HeaderComponent implements OnInit {
-  //images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
-  //images = [1, 2, 3].map(() => '../../assets/img/header?random&t=${Math.random()}');
-  images = ["../../assets/img/header1.jpg","../../assets/img/header1px300.jpg","../../assets/img/header1px400.jpg","../../assets/img/header1px500.jpg","../../assets/img/header1px600.jpg","../../assets/img/header1px700.jpg","../../assets/img/header1px800.jpg","../../assets/img/header1px1000.jpg","../../assets/img/header1px1200.jpg",
-  "../../assets/img/header2.jpg","../../assets/img/header2px300.jpg","../../assets/img/header2px400.jpg","../../assets/img/header2px500.jpg","../../assets/img/header2px600.jpg","../../assets/img/header2px700.jpg","../../assets/img/header2px800.jpg","../../assets/img/header2px1000.jpg","../../assets/img/header2px1200.jpg",
-  "../../assets/img/headerMemoriaal.jpg","../../assets/img/headerMemoriaalpx300.jpg","../../assets/img/headerMemoriaalpx400.jpg","../../assets/img/headerMemoriaalpx500.jpg","../../assets/img/headerMemoriaalpx600.jpg","../../assets/img/headerMemoriaalpx700.jpg","../../assets/img/headerMemoriaalpx800.jpg","../../assets/img/headerMemoriaalpx900.jpg","../../assets/img/headerMemoriaalpx1000.jpg","../../assets/img/headerMemoriaalpx1500.jpg"]
-  
+  sizes = [
+    '',
+    'px300',
+    'px400',
+    'px500',
+    'px600',
+    'px700',
+    'px800',
+    'px1000',
+    'px1200'
+  ];
+  images = [
+    this.sizes.map((size) => `../../assets/img/header-covers/header1${size}.jpg`),
+    this.sizes.map((size) => `../../assets/img/header-covers/header2${size}.jpg`),
+    this.sizes.map((size) => `../../assets/img/header-covers/headerMemoriaal${size}.jpg`),
+  ];
+
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
 }
